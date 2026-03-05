eb68368940a37b197831b9b4c3c9d4268136faed
Azuma Okumura <indexlove0815@icloud.com>
2026-03-06 06:38:56 +0900

    [summary with Antigravity agent] Source Han Sans JP ExtraLightフォントの導入、全般的なウェイト調整(-100)、およびApp Hosting向けにテスト関連ファイルのビルド除外設定を追加

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -48,7 +48,20 @@ export default async function RootLayout(props: { children: React.ReactNode }) {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Oxanium:wght@200..800&display=swap" rel="stylesheet" />
-
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(d) {
+                var config = {
+                  kitId: 'jtq7dsy',
+                  scriptTimeout: 3000,
+                  async: true
+                },
+                h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\\bwf-loading\\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+              })(document);
+            `,
+          }}
+        />
       </head>
       <body>
         <CustomCursor />
