163c44c0cc96262dc24b1616314ea3cb88684bb8
Azuma Okumura <indexlove0815@icloud.com>
2026-03-08 02:33:21 +0900

    [Antigravity] データベーススキーマとデータの同期完了 (local -> production)

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,21 @@
 import * as migration_20260226_111056_initial_setup from './20260226_111056_initial_setup';
+import * as migration_20260304_050000_add_posts_drafts from './20260304_050000_add_posts_drafts';
+import * as migration_20260307_171744_sync_local_to_prod_schema from './20260307_171744_sync_local_to_prod_schema';
 
 export const migrations = [
   {
     up: migration_20260226_111056_initial_setup.up,
     down: migration_20260226_111056_initial_setup.down,
-    name: '20260226_111056_initial_setup'
+    name: '20260226_111056_initial_setup',
+  },
+  {
+    up: migration_20260304_050000_add_posts_drafts.up,
+    down: migration_20260304_050000_add_posts_drafts.down,
+    name: '20260304_050000_add_posts_drafts',
+  },
+  {
+    up: migration_20260307_171744_sync_local_to_prod_schema.up,
+    down: migration_20260307_171744_sync_local_to_prod_schema.down,
+    name: '20260307_171744_sync_local_to_prod_schema'
   },
 ];
